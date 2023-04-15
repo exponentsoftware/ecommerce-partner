a52ca7c22ccbfc1d744713976c2535f20419dccc
Rahul Rajput <rahulrajput21112@gmail.com>
2023-04-15 17:39:26 +0530

    Added New Product page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,25 +1,27 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
-import { BiHomeAlt2, BiUser } from 'react-icons/bi'
+import { getToken } from "/Functions/getToken";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { BiUser, BiDialpad } from 'react-icons/bi'
+import {MdAdd} from 'react-icons/md'
 
 export default function Navbar() {
-    const router = useRouter()
+    const router = useRouter();
+    const [token, setToken] = useState('')
+    useEffect(() => {
+        setToken(getToken())
+    }, [])
     return (
         <div className={`flex flex-row fixed z-20 gap-2 sm:gap-5 w-full items-center bg-red-500 justify-center p-3 box-border`}>
             <Link href='/' className={`text-white sm:block font-medium mr-auto text-base`}><span className="font-semibold text-lg">EASY</span> ORDER</Link>
-
-            <button className='p-2 sm:py-1 sm:px-3 flex flex-row justify-center items-center gap-1 rounded-full border-[0.14rem] border-white hover:bg-red-500  hover:text-white uppercase font-semibold bg-white'>
-                {router.pathname.includes('login') || router.pathname.includes('register') || router.pathname.includes('/account') ?
-                    <>
-                        <BiHomeAlt2 className="text-xl" />
-                        <span className="text-sm hidden sm:block">Home</span>
-                    </>
-                    :
-                    <>
-                        <BiUser className="text-xl" />
-                        <span className="text-sm hidden sm:block">Account</span>
-                    </>}
-            </button>
+            {token && !router.pathname.includes('/add') && <Link href={`/account`} className='p-2 sm:py-1 sm:px-3 flex flex-row justify-center items-center gap-1 rounded-full border-[0.14rem] border-white hover:bg-red-500  hover:text-white uppercase font-medium bg-white'>
+                <MdAdd className="text-xl" />
+                <span className="text-sm hidden sm:block">New Product</span>
+            </Link>}
+            <Link href={`/account`} className='p-2 sm:py-1 sm:px-3 flex flex-row justify-center items-center gap-1 rounded-full border-[0.14rem] border-white hover:bg-red-500  hover:text-white uppercase font-medium bg-white'>
+                <BiUser className="text-xl" />
+                <span className="text-sm hidden sm:block">Account</span>
+            </Link>
         </div>
     )
 }
